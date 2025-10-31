92f6a4fc1a7fff7204b5a2a41a3465762aa41e3b
Light0777 <lightyagami7502@gmail.com>
2025-10-31 23:38:02 +0530

    Delete server directory

--- server/associations.js
+++
@@ -1,8 +0,0 @@
-const User = require('./models/user');
-const Tenant = require('./models/tenant');
-
-// Define relationships
-User.belongsTo(Tenant, { foreignKey: 'tenantId', onDelete: 'CASCADE' });
-Tenant.hasMany(User, { foreignKey: 'tenantId' });
-
-module.exports = { User, Tenant };
--- server/secrets.js
+++
@@ -1,7 +0,0 @@
-// secrets.js
-// server/secrets.js
-const secrets = {
-  dbUri: "mongodb+srv://Lightyagami:7502922716@sociallink.cuv9k7b.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=SocialLink"
-};
-
-export const getSecret = (key) => secrets[key];
